2dcbb365b384092ca2a3950d619603b47c0dd438
Guy McKechnie <mckechnieguy.mg@gmail.com>
2025-01-28 20:30:33 +0200

    Add Firebase dependency, update .gitignore for environment files, and clean up imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { motion } from "framer-motion";
+// Components
 import Home from "./Pages/Home.jsx";
 import GradeCollection from "./Pages/GradeCollection.jsx";
 import Product from "./Pages/Product.jsx";
@@ -18,9 +22,6 @@ import TOS from "./Pages/TOS.jsx";
 import ReturnsPolicy from "./Pages/ReturnsPolicy.jsx";
 import NotFound from "./Pages/NotFound.jsx";
 import AboutUs from "./Pages/AboutUs.jsx";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
-import { motion } from "framer-motion";
 import { LoadingAnimation } from "./Components/Utilities/LoadingAnimation.jsx";
 
 function App() {
