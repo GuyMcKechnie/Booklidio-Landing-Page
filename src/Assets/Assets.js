ee2418b855a513da9f5a969a731cb40ae98ad804
Guy McKechnie <mckechnieguy.mg@gmail.com>
2025-02-12 21:30:09 +0200

    Removed old source files, assets, and unused components. Replaced contact page wwith WhatsApp contact. Refactoring.

--- src/Assets/Assets.js
+++ src/Assets/Assets.js
@@ -7,35 +7,8 @@ export const assets = {
   buy,
   sell,
   faq,
-  studyGuide1,
-  studyGuide2,
-  studyGuide3,
-  studyGuide4,
   doodle,
 };
-
-export const products = [
-  {
-    id: uuid4(),
-    image: accountingImage,
-    name: "Accounting",
-    price: 120,
-    grade: 2,
-    subject: "Afrikaans",
-    description:
-      "By ensuring that deliveryFee is properly declared and the ShopContextProvider component is correctly defined and used, you should be able to resolve the defaultValue is not defined error. This setup ensures that the products state is accessible within the Collections component.",
-  },
-  {
-    id: uuid4(),
-    image: accountingImage,
-    name: "Accounting",
-    price: 120,
-    grade: 1,
-    subject: "English",
-    description:
-      "By ensuring that deliveryFee is properly declared and the ShopContextProvider component is correctly defined and used, you should be able to resolve the defaultValue is not defined error. This setup ensures that the products state is accessible within the Collections component.",
-  },
-];
 
 import hero from "./hero.png";
 import header from "./header.png";
@@ -43,9 +16,4 @@ import features from "./features.png";
 import buy from "./buy.png";
 import sell from "./sell.png";
 import faq from "./faq.png";
-import accountingImage from "./Subjects/acc-subject.png";
-import studyGuide1 from "./Subjects/study-guide-1.jpg";
-import studyGuide2 from "./Subjects/study-guide-2.jpg";
-import studyGuide3 from "./Subjects/study-guide-3.jpg";
-import studyGuide4 from "./Subjects/study-guide-4.jpg";
 import doodle from "./doodle.svg";
